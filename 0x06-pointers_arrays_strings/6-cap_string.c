e8117c05a16657a427e681f2bfa9fa60c61f09fc
Hillary-kirwa <hillarycherutich12@gmail.com>
2022-12-22 03:41:46 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - capitalizes all words of a string.
  * @str: The string to be capitalized.
@@ -29,7 +28,6 @@ char *cap_string(char *str)
 	          (str[index - 1] == '{' ||
 		  (str[index - 1] == '}' ||
 		  str[index] -= 32;
-
 		 index++;
 		 }
 
