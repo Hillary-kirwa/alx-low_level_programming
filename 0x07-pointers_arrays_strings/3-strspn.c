a58238c5bd8e044b50b74afff4baa05adfdcb251
Hillary-kirwa <hillarycherutich12@gmail.com>
2023-01-03 01:09:12 -0800

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strspn - prints the consecutive characters of s1 that are i s2.
+ * @s:source string
+ * @accept: searching string
+ * Return: new string.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (j = 0; *(s + j); i++)
+	{
+		for (i = 0; *(accept = i++)
+				{
+				if (*(s + j) == *(accept + i))
+				break;
+				}
+				if (*(accept + i) == '\0')
+				break;
+				}
+				return (j);
